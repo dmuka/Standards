7995b41b16ef8d22db50316bb257a514593c306b
Dzmitry_Belavusau <Dzmitry_Belavusau1@epam.com>
2022-09-25 00:15:59 +0300

    Add sector model

---
+++ Standards/Models/Department/Sector.cs
@@ -0,0 +1,11 @@
+﻿namespace Standards.Models.Department
+{
+    public class Sector
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string ShortName { get; set; } = null!;
+        public int DepartmentId { get; set; }
+        public string Comments { get; set; } = null!;
+    }
+}
