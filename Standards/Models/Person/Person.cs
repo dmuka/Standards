32a6fad177c553b63f6c7e72d21f1c82d2f99e58
Dzmitry_Belavusau <Dzmitry_Belavusau1@epam.com>
2022-09-25 00:10:44 +0300

    Add Person model

--- Standards/Models/Person/Person.cs
+++ Standards/Models/Person/Person.cs
@@ -2,5 +2,16 @@
 {
     public class Person
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string MiddleName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public int PositionId { get; set; }
+        public DateOnly BirthdayDate { get; set; }
+        public int DepartmentId { get; set; }
+        public int SectorId { get; set; }
+        public int RoleId { get; set; }
+        public string Comments { get; set; } = null!;
     }
 }
