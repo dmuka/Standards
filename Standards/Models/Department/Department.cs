be49c414ca318d0770e020a805e145df2106a36e
Dzmitry_Belavusau <Dzmitry_Belavusau1@epam.com>
2022-09-25 00:17:32 +0300

    Add department model

---
+++ Standards/Models/Department/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace Standards.Models.Department
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string ShortName { get; set; } = null!;
+        public string Comments { get; set; } = null!;
+    }
+}
