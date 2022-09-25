8fd4fa4eb6cc56734b09e984b232ab32c5fea5f3
Dzmitry_Belavusau <Dzmitry_Belavusau1@epam.com>
2022-09-25 18:13:42 +0300

    Add services journal model

---
+++ Standards/Models/Services/ServicesJournal.cs
@@ -0,0 +1,12 @@
+﻿namespace Standards.Models.Services
+{
+    public class ServicesJournal
+    {
+        public int Id { get; set; }
+        public int StandardId { get; set; }
+        public int PersonId { get; set; }
+        public int ServiceId { get; set; }
+        public DateOnly Date { get; set; }
+        public string Comments { get; set; } = null!;
+    }
+}
