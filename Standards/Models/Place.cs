1c3cd3aaf9d14724a4d25c372d8f5e08a493d32d
Dzmitry_Belavusau <Dzmitry_Belavusau1@epam.com>
2022-09-25 18:23:09 +0300

    Add place and verification journal models

---
+++ Standards/Models/Place.cs
@@ -0,0 +1,9 @@
+﻿namespace Standards.Models
+{
+    public class Place
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Comments { get; set; } = null!;
+    }
+}
---
+++ Standards/Models/VerificationJournal.cs
@@ -0,0 +1,14 @@
+﻿namespace Standards.Models
+{
+    public class VerificationJournal
+    {
+        public int Id { get; set; }
+        public int StandardId { get; set; }
+        public int PlaceId { get; set; }
+        public DateOnly Date { get; set; }
+        public DateOnly ValidTo { get; set; }
+        public string SertificateId { get; set; } = null!;
+        public string SertificateImage { get; set; } = null!;
+        public string Comments { get; set; } = null!;
+    }
+}
