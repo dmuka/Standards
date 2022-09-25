5c9fb556ad90dd1960db1cb54c5c95a757c4538f
Dzmitry_Belavusau <Dzmitry_Belavusau1@epam.com>
2022-09-25 17:48:42 +0300

    Add standard model

---
+++ Standards/Models/Standards/Standard.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Standards.Models.Standards
+{
+    public class Standard
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public int WorkPlaceId { get; set; }
+        [Required]
+        public int ResponcibleId { get; set; }
+        public IList<Characteristic> Characteristics { get; set; } = new List<Characteristic>();
+        public string ImagePath { get; set; } = null!;
+        public int VerificationInterval { get; set; }
+        public int CalibrationInterval { get; set; }
+        public IList<Service> Services { get; set; } = new List<Service>();
+        public string Comments { get; set; } = null!;
+    }
+}
