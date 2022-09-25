f541abb67d590f2ab2ee13297af95140ee1b5469
Dzmitry_Belavusau <Dzmitry_Belavusau1@epam.com>
2022-09-25 18:08:24 +0300

    Add service model

---
+++ Standards/Models/Services/Service.cs
@@ -0,0 +1,13 @@
+﻿namespace Standards.Models.Services
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int TypeId { get; set; }
+        public IList<Material> Materials { get; set; } = new List<Material>();
+        public IList<double> MaterialsQuantities { get; set; } = new List<double>();
+        public IList<Unit> MaterialsUnits { get; set; } = new List<Unit>();
+        public string Comments { get; set; } = null!;
+    }
+}
