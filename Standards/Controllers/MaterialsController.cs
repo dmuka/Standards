3bcc1d4b134a198e3bd6b9adb12787160ff0e4a2
Dmitry Belousov <belavusdm@gmail.com>
2024-11-06 21:54:02 +0300

    Add handlers and controller for material entity

---
+++ Standards/Controllers/MaterialsController.cs
@@ -0,0 +1,80 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Standards.Core.CQRS.Common.GenericCRUD;
+using Standards.Core.CQRS.Materials;
+using Standards.Core.Models.DTOs;
+using Standards.Core.Models.Services;
+using Standards.Infrastructure.Filter.Implementations;
+
+namespace Standards.Controllers;
+
+[Route("api/materials")]
+[ApiController]
+public class MaterialsController(ISender sender) : ControllerBase
+{
+    [HttpGet]
+    [Route("list")]
+    public async Task<IActionResult> GetMaterials()
+    {
+        var query = new GetAll.Query();
+
+        var result = await sender.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<IActionResult> GetMaterial(int id)
+    {
+        var query = new GetById.Query<Material>(id);
+
+        var result = await sender.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Route("add")]
+    public async Task<IActionResult> CreateMaterial([FromBody] MaterialDto material)
+    {
+        var query = new Create.Query(material);
+
+        var result = await sender.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpPut]
+    [Route("edit")]
+    public async Task<IActionResult> EditMaterial([FromBody] MaterialDto material)
+    {
+        var query = new Edit.Query(material);
+            
+        var result = await sender.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    [Route("delete/{id:int}")]
+    public async Task<IActionResult> DeleteMaterial(int id)
+    {
+        var query = new Delete.Query<Material>(id);
+
+        var result = await sender.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Route("filter")]
+    public async Task<IActionResult> GetMaterialsByFilter([FromBody] QueryParameters parameters)
+    {
+        var query = new GetFiltered<Material>.Query(parameters);
+
+        var result = await sender.Send(query);
+
+        return Ok(result);
+    }
+}
---
+++ Standards/Core/Models/DTOs/MaterialDto.cs
@@ -0,0 +1,6 @@
+namespace Standards.Core.Models.DTOs;
+
+public class MaterialDto : BaseEntity
+{
+    public int UnitId { get; set; }
+}
