268e1c41e718abdea0345340789cfb745fe3321b
Dmitry Belousov <belavusdm@gmail.com>
2024-11-08 00:14:28 +0300

    Add tests for CategoriesController

---
+++ StandardsTests/Controllers/CategoriesControllerTests.cs
@@ -0,0 +1,118 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Standards.Controllers;
+using Standards.Core.CQRS.Common.GenericCRUD;
+using Standards.Core.Models.Persons;
+using Standards.CQRS.Tests.Common;
+
+namespace Standards.CQRS.Tests.Controllers;
+
+[TestFixture]
+public class CategoriesControllerTests : BaseTestFixture
+{
+    private Mock<ISender> _senderMock;
+    private CategoriesController _controller;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _senderMock = new Mock<ISender>();
+        _controller = new CategoriesController(_senderMock.Object);
+    }
+
+    [Test]
+    public async Task GetCategories_ShouldReturnOkWithCategories()
+    {
+        // Arrange
+        var categories = Categories;
+        _senderMock
+            .Setup(s => s.Send(It.IsAny<GetAllBaseEntity.Query<Category>>(), default))
+            .ReturnsAsync(categories);
+
+        // Act
+        var result = await _controller.GetCategories();
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That((List<Category>)okResult.Value, Is.EquivalentTo(categories));
+    }
+
+    [Test]
+    public async Task GetCategory_ShouldReturnOkWithCategory()
+    {
+        // Arrange
+        var category = Categories[0];
+        _senderMock
+            .Setup(s => s.Send(It.IsAny<GetById.Query<Category>>(), default))
+            .ReturnsAsync(category);
+
+        // Act
+        var result = await _controller.GetCategory(1);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That((Category)okResult.Value, Is.EqualTo(category));
+    }
+
+    [Test]
+    public async Task CreateCategory_ShouldReturnOkWithCreatedCategory()
+    {
+        // Arrange
+        var category = Categories[0];
+        _senderMock
+            .Setup(s => s.Send(It.IsAny<CreateBaseEntity.Query<Category>>(), default))
+            .ReturnsAsync(category.Id);
+
+        // Act
+        var result = await _controller.CreateCategory(category);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That((int)okResult.Value, Is.EqualTo(category.Id));
+    }
+
+    [Test]
+    public async Task EditCategory_ShouldReturnOkWithEditedCategory()
+    {
+        // Arrange
+        var category = Categories[0];
+        _senderMock
+            .Setup(s => s.Send(It.IsAny<EditBaseEntity.Query<Category>>(), default))
+            .ReturnsAsync(1);
+
+        // Act
+        var result = await _controller.EditCategory(category);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That((int)okResult.Value, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task DeleteCategory_ShouldReturnOkWithDeletedCategory()
+    {
+        // Arrange
+        var category = Categories[0];
+        _senderMock
+            .Setup(s => s.Send(It.IsAny<Delete.Query<Category>>(), default))
+            .ReturnsAsync(1);
+
+        // Act
+        var result = await _controller.DeleteCategory(1);
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That((int)okResult.Value, Is.EqualTo(1));
+    }
+}
