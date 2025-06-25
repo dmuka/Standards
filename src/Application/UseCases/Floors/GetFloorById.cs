c68452b78175a03560fbd80f3497af63e4f36ea0
Dmitry Belousov <belavusdm@gmail.com>
2025-06-25 22:25:50 +0300

    Add GetFloorById

---
+++ src/Application/UseCases/Floors/GetFloorById.cs
@@ -0,0 +1,26 @@
+using Domain.Aggregates.Floors;
+using Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Floors;
+
+public class GetFloorById
+{
+    public class Query : IRequest<Floor?>
+    {
+        public required FloorId FloorId { get; set; }
+    }
+    
+    public class QueryHandler(ApplicationDbContext dbContext) : IRequestHandler<Query, Floor?>
+    {
+        public async Task<Floor?> Handle(Query query, CancellationToken cancellationToken)
+        {
+            var housing = await dbContext.Floors
+                .AsNoTracking()
+                .FirstOrDefaultAsync(housing => housing.Id == query.FloorId, cancellationToken);
+
+            return housing;
+        }
+    }
+}
